6bc625b01188392c8ef4ed04a3b3ba2443759b96
mcespo <mcespo@gmail.com>
2021-10-14 12:57:12 -0500

    svelte nelify

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
 import { mdsvex } from 'mdsvex';
 import mdsvexConfig from './mdsvex.config.js';
 import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     extensions: ['.svelte', ...mdsvexConfig.extensions],
 
     kit: {
-        // hydrate the <div id="svelte"> element in src/app.html
+        adapter: adapter(),
         target: '#svelte',
     },
 
